305f0eee14c9595ba800275595a2aaf206ed30a2
Kub3kk <kuba44640@gmail.com>
2026-02-16 22:17:28 +0100

    Added navigation tab

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,40 @@
+import { Github, Youtube, ChartColumnBig } from 'lucide-react';
 import './index.css'
 
-const navBar = () => {
+const NavBar = () => {
   return (
     <>
-      <div>
+      <div className="flex justify-between items-center m-4">
+        <div className="flex space-x-4">
+          <a href="https://github.com/Kub3kkk/tekstdle">
+            <Github color="white" size={48} />
+          </a>
+          <a href="">
+            <Youtube color="white" size={48} />
+          </a>
+        </div>
 
+        <div>
+          <h1 className='text-5xl text-white'>Tekstdle.pl</h1>
+        </div>
+
+        <div>
+          <ChartColumnBig color='white' size={48}/>
+        </div>
       </div>
+
+    </>
+  )
+}
+
+const Verse = () => {
+  
+}
+
+const Pannel = () => {
+  return (
+    <>
+
     </>
   )
 }
@@ -13,7 +42,8 @@ const navBar = () => {
 const App = () => {
   return (
     <>
-
+      <NavBar />
+      
     </>
   )
 }
