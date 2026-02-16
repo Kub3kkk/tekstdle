21ad6b687bc9b417bebc4cb3b200110d8c10e52d
Kub3kk <kuba44640@gmail.com>
2026-02-16 21:18:46 +0100

    Project setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import './index.css'
+
+const navBar = () => {
+  return (
+    <>
+      <div>
+
+      </div>
+    </>
+  )
+}
+
+const App = () => {
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
